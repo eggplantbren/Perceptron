445f7134ef66df3ea1be7928cbc805dd24cdc819
Brendon Brewer <brendon.brewer@gmail.com>
2015-05-23 14:27:58 +1200

    run

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char** argv)
 {
 	Data::get_instance().load("clocks.txt");
 
-//	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
-//	sampler.run();
+	MTSampler<MyModel> sampler = setup_mt<MyModel>(argc, argv);
+	sampler.run();
 	return 0;
 }
 
