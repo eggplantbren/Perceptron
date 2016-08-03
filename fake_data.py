b745bff9281b3c813cd19889bee9852e52b4a706
Brendon Brewer <brendon.brewer@gmail.com>
2016-08-03 19:43:38 +1200

    WOrks better

--- fake_data.py
+++ fake_data.py
@@ -3,12 +3,12 @@ from pylab import *
 seed(0)
 
 # Some inputs
-N = 1000
+N = 100
 x = 3*randn(N)
 
 # An output
 f = x - cos(x) + (0.1 + 0.1*abs(x))*randn(N)
-f[x > 2] -= 2.0
+f[x > 2] -= 5.0
 plot(x, f, "ko", markersize=3, alpha=0.2)
 savetxt('fake_data.txt', vstack([x, f]).T, header="1 1")
 
