eae8cfdb767a3a11fbee565275aa540f5b13c0bb
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-26 10:16:16 +1300

    Consistency with other plot

--- display.py
+++ display.py
@@ -7,10 +7,11 @@ posterior_sample = np.atleast_2d(dn4.my_loadtxt("posterior_sample.txt"))
 
 x = np.linspace(-10.0, 10.0, 2001)
 
+plt.figure(figsize=(13, 4))
 for i in range(0, posterior_sample.shape[0]):
     plt.plot(x, posterior_sample[i, :], "g-", alpha=0.1)
     plt.hold(True)
 
-plt.plot(data[:,0], data[:,1], "k.", markersize=10, alpha=0.2)
+plt.plot(data[:,0], data[:,1], "ko", markersize=5, alpha=0.1)
 plt.show()
 
