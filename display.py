2c02178c0c44ba7968df354e8765b4c7b1c1c4f6
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-10-13 12:06:03 +1300

    A nicer plot

--- display.py
+++ display.py
@@ -7,10 +7,12 @@ posterior_sample = np.atleast_2d(dn4.my_loadtxt("posterior_sample.txt"))
 
 x = np.linspace(-20.0, 20.0, 2001)
 
-plt.figure(figsize=(13, 4))
-for i in range(0, posterior_sample.shape[0]):
+plt.figure(figsize=(13, 6))
+for i in range(0, min(100, posterior_sample.shape[0])):
     plt.plot(x, posterior_sample[i, :], "g-", alpha=0.1)
 
-plt.plot(data[:,0], data[:,1], "ko", markersize=5, alpha=0.1)
+plt.plot(data[:,0], data[:,1], "ko", markersize=3, alpha=0.2)
+plt.xlabel("x", fontsize=16)
+plt.ylabel("y", fontsize=16)
 plt.show()
 
